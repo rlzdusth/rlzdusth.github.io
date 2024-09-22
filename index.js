d0a769e82983034db9101e869ea5603e54120e01
rlzdusth <iamrgbs@proton.me>
2024-09-22 13:27:32 +0800

    Adding some initial features

---
+++ index.js
@@ -0,0 +1,34 @@
+
+//JSON CODE
+// const display = document.getElementById('display')
+
+// let myData = []
+
+// fetch('users.json')
+//     .then(res => res.json())
+//     .then(data => {
+//         myData = data
+//         displayData()
+//     })
+//     .catch(err => {
+//         console.error(err)
+//     })
+
+// function displayData() {
+//     myData.forEach(e => {
+//         display.innerHTML += JSON.stringify(e.name) + "<br>"
+//     })
+// }
+
+
+//POPUP SHIT
+// const popup = document.getElementById('popup')
+// const closePopup = document.getElementById('close')
+// const showPopup = document.getElementById('show')
+
+// function TogglePopup() {
+//     popup.classList.toggle('show')
+// }
+
+// closePopup.addEventListener("click", TogglePopup)
+// showPopup.addEventListener("click", TogglePopup)
